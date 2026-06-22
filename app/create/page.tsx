87eb057d9922af37b47c7cf0a9a336edafe46e22
Gouravsaha12 <gouravsaha123098@gmail.com>
2026-06-22 17:00:37 +0530

    create form ui created for /create page

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -2,8 +2,32 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className="px-4 md:px-8 py-4 flex flex-co">
-      create
+    <div className='w-screen h-full flex justify-center items-center'>
+        <form action="" className="px-4 py-4 flex flex-col mt-6 w-[96%] md:w-[70%] lg:w-[40%] gap-4">
+
+            <div className="relative z-0 w-full mb-5 group">
+                <input type="text" name="title" className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer" placeholder=" " required />
+                <label htmlFor="title" className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Title</label>
+            </div>
+
+            <div className="relative z-0 w-full mb-5 group">
+                <input type="text" name="desc" className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer" placeholder=" " required />
+                <label htmlFor="desc" className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Any thoughts on your recc ?</label>
+            </div>
+
+            <div className="relative z-0 w-full mb-5 group">
+                <input type="text" name="url" className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer" placeholder=" " required />
+                <label htmlFor="url" className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">URL for your recc ?</label>
+            </div>
+
+            <div className="relative z-0 w-full mb-5 group">
+                <input type="text" name="type" className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer" placeholder=" " required />
+                <label htmlFor="type" className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">What is this ? A yt video or a book or something else ?</label>
+            </div>
+            
+            <button type="submit" className="border-1 border-white cursor-pointer hover:bg-zinc-800 rounded-sm py-2">Submit</button>
+
+        </form>
     </div>
   )
 }
