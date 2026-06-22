a168286f6c806ae3cce7ec35bbad88974c0e9dc1
Gouravsaha12 <gouravsaha123098@gmail.com>
2026-06-22 15:28:58 +0530

    navbar fixed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,15 +8,18 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center px-4 md:px-8 border-b-1 border-zinc-900'>
-      <div>CampusMarket</div>
+      <div>DeepRecc</div>
       <div className='flex gap-6'>
         <Link href='/' className={`py-2 + ${path === '/' ? 'border-b-2 border-white' : ''}`} >
             home
         </Link>
-        <Link href='/' className={`py-2 + ${path === '/requests' ? 'border-b-2 border-white' : ''}`}>
-            requests
+        <Link href='/following' className={`py-2 + ${path === '/requests' ? 'border-b-2 border-white' : ''}`}>
+            following
         </Link>
-        <Link href='/' className={`py-2 + ${path === '/profile' ? 'border-b-2 border-white' : ''}`}>
+        <Link href='/reccs' className={`py-2 + ${path === '/requests' ? 'border-b-2 border-white' : ''}`}>
+            my reccs
+        </Link>
+        <Link href='/profile' className={`py-2 + ${path === '/profile' ? 'border-b-2 border-white' : ''}`}>
             profile
         </Link>
       </div>
